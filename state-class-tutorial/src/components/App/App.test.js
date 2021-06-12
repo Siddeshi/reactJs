91a3bc17c6ce539ed125882956dda8a358ac38d4
siddesh <siddusrb@gmail.com>
2021-06-12 14:54:09 +0530

    corrected test case

--- state-class-tutorial/src/components/App/App.test.js
+++ state-class-tutorial/src/components/App/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ice creame/i);
   expect(linkElement).toBeInTheDocument();
 });
